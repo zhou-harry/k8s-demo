0b936971a2aa0b20597a90f83af7e3d69b120592
zhouhong <hzhou@tplhk.cntaiping.com>
2019-07-24 14:19:39 +0800

    commit

--- k8s-demo/deploy/deploy.sh
+++ k8s-demo/deploy/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+#unable to recognize "k8s-demo.yml": Get http://localhost:8080/api?timeout=32s: dial tcp [::1]:8080: connect: connection refused
 
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+#mkdir -p $HOME/.kube
+#sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+#sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 cd /harry/deploy/project
 
