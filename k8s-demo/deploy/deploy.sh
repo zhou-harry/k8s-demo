c92b50e25ebb1df864a6b69c09f42a2d6b6bf45d
zhouhong <hzhou@tplhk.cntaiping.com>
2019-07-24 14:34:03 +0800

    commit

--- k8s-demo/deploy/deploy.sh
+++ k8s-demo/deploy/deploy.sh
@@ -5,6 +5,5 @@
 #sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 #sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-#cd /harry/deploy/project
-
+cd /harry/deploy/project
 kubectl apply -f k8s-demo.yaml
