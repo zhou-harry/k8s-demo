59cdd838bcd678a9c7dd6f203544cd86858bb2ad
zhouhong <hzhou@tplhk.cntaiping.com>
2019-07-24 10:29:36 +0800

    commit

--- k8s-demo/deploy/deploy.sh
+++ k8s-demo/deploy/deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 echo "ssh到k8s服务器"
-ssh -tt 192.168.88.130
+ssh -tt root@192.168.88.130 "cd /harry/deploy/project; pwd"
 echo "进入部署目录"
-cd /harry/deploy/project
-pwd
+#cd /harry/deploy/project
 #echo "发布"
 #kubectl apply -f k8s-demo.yaml
-echo "退出k8s服务器"
-exit
+#echo "退出k8s服务器"
+#exit
