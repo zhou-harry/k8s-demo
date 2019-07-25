f309e1d55e1caa8c73cdc3b6805264ed71bea53d
zhouhong <hzhou@tplhk.cntaiping.com>
2019-07-25 14:47:30 +0800

    commit

--- k8s-demo/src/main/java/com/harry/demo/controller/DemoController.java
+++ k8s-demo/src/main/java/com/harry/demo/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.harry.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
+    @Value("${spring.application.name}")
+    private String applicationName;
+
     @GetMapping("index")
     public String index() {
-        return "k8s deploy intergration was success！";
+        System.out.printf("后台服务 %s 启动成功！", applicationName);
+        return "k8s deploy intergration was success！the application name is ："+applicationName;
     }
 
 }
